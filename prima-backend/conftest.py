1158f233cd18ac032cc4e608bd210cc1f7277cf7
mihsan02 <muhammadihsanudin02@gmail.com>
2026-05-25 15:53:22 +0000

    fix: stress-test regresi pytest - TESTING guard, indentasi _db.close, PROPAGATE_EXCEPTIONS

--- prima-backend/conftest.py
+++ prima-backend/conftest.py
@@ -8,6 +8,7 @@ from app import app as flask_app
 @pytest.fixture
 def client():
     flask_app.config['TESTING'] = True
+    flask_app.config['PROPAGATE_EXCEPTIONS'] = True
     with flask_app.test_client() as c:
         original_open = c.open
         def patched_open(*args, **kwargs):
